c3b02ef1fa15a9a99272f59a52af5d1109b4b14e
Javeedsly <cavidsly@gmail.com>
2026-05-13 00:44:19 +0400

    ....

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Səhifənin daha sürətli yüklənməsi üçün sıxılmanı aktiv edir
   compress: true, 
+  
+  // Şəkilləri ən yeni və yüngül formatlara (WebP və AVIF) çevirir
   images: {
     formats: ['image/avif', 'image/webp'],
   },
